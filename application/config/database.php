11da463d313b07d7e46a6c241da6c71314665c65
fachiri <fachrycooles@gmail.com>
2022-06-02 21:48:44 +0800

    change db

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '127.0.0.1',
+	'username' => 'u422015075_presensi_user',
+	'password' => 'Admin@haycab123',
 	'database' => 'u422015075_presensi_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
